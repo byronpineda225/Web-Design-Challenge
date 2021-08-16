2a357972b6c1aab4c4dbf4b5c95b709877ad3466
byronpineda225 <84112237+byronpineda225@users.noreply.github.com>
2021-08-15 22:32:43 -0400

    Add files via upload

---
+++ WebVisualizations/data.py
@@ -0,0 +1,13 @@
+# Read in a csv file and write out to html
+import pandas as pd
+
+# Read the csv file in for weather dashboard
+df = pd.read_csv('Resources/cities.csv')
+
+# Get an HTML format of a dataframe by 
+# using the DataFrame.to_html() method. 
+df.to_html('data.html', index=False)
+
+# Assign and print
+table = df.to_html()
+print(table)
